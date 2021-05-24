a666bd26c28de61f3d57dd921f130d89bb325a6a
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-05-24 17:17:01 +0530

    UI design finished

--- src/controller/DecryptionFormController.java
+++ src/controller/DecryptionFormController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class DecryptionFormController {
+    public JFXTextField txtKey;
+    public JFXTextField txtOutput;
+    public JFXTextField txtCipherText;
+
+    public void btnDecrypt_OnAction(ActionEvent actionEvent) {
+    }
 }
--- src/controller/EncryptionFormController.java
+++ src/controller/EncryptionFormController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class EncryptionFormController {
+    public JFXTextField txtText;
+    public JFXTextField txtKey;
+    public JFXTextField txtOutput;
+
+    public void btnEncrypt_OnAction(ActionEvent actionEvent) {
+    }
 }
